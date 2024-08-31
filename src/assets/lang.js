ff0466cd94d6d2cc82b99f043d1b8b7ee076575c
Andrew <aldortheold@gmail.com>
2024-08-31 16:08:34 +1000

    improved language autodetect

--- src/assets/lang.js
+++ src/assets/lang.js
@@ -1,7 +1,7 @@
 import languages from './json/languages.json';
 
 export default function lang(settings) {
-    if ((settings.language === null) || (settings === undefined)) {
+    if ((settings.language !== 0) || (!settings.language)) {
         if (languages.includes(window.navigator.language.slice(0, 2))) {
             return languages.indexOf(window.navigator.language.slice(0, 2));
         }
