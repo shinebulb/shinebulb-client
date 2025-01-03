7c49f8a3744d3fdc7907f12f74e1d2e2acd7a5d0
Andrew <aldortheold@gmail.com>
2025-01-03 14:49:12 +1000

    improved change-password button text

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -75,7 +75,7 @@ function Profile({ settings, bulb }) {
                     </div>
                 </>
             }</>}
-            <button className="change-password">change password</button>
+            <button className="change-password">{text[settings.language].changePassword}</button>
             <dialog ref={copyModal} className="copy-modal">
                 <h2>{text[settings.language].pickCopyColors[0]}</h2>
                 <hr />
